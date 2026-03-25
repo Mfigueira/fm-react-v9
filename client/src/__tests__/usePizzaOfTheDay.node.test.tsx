df9853c9d3c9e33cd1bca46e365521f9892d341d
Manuel Figueira <manuel.figueira.9@gmail.com>
2026-03-25 14:16:35 -0300

    fix hook test

--- client/src/__tests__/usePizzaOfTheDay.node.test.tsx
+++ client/src/__tests__/usePizzaOfTheDay.node.test.tsx
@@ -3,9 +3,12 @@ import { renderHook, waitFor } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
 import type { Pizza } from "../types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const fetchMocker = createFetchMock(vi);
 fetchMocker.enableMocks();
+
+const queryClient = new QueryClient({});
 
 const testPizza: Pizza = {
   id: "calabrese",
@@ -19,15 +22,23 @@ const testPizza: Pizza = {
 
 test("to be null on initial load", async () => {
   fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
-  const { result } = renderHook(() => usePizzaOfTheDay());
-  expect(result.current).toBeNull();
+  const { result } = renderHook(() => usePizzaOfTheDay(), {
+    wrapper: ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+  expect(result.current).toBeUndefined();
 });
 
 test("to call the API and give back the pizza of the day", async () => {
   fetchMocker.mockResponseOnce(JSON.stringify(testPizza));
-  const { result } = renderHook(() => usePizzaOfTheDay());
+  const { result } = renderHook(() => usePizzaOfTheDay(), {
+    wrapper: ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
   await waitFor(() => {
     expect(result.current).toEqual(testPizza);
   });
-  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
+  expect(fetchMocker).toHaveBeenCalledWith("/api/pizza-of-the-day");
 });
